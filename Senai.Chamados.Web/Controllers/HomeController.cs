9268a13456e4a14dfc74a42e329d46867eb20f14
Madson Oliveira <madson.bass@hotmail.com>
2018-06-21 18:01:50 -0300

    Adicionar arquivos de projeto.

---
+++ Senai.Chamados.Web/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Senai.Chamados.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Index Controller";
+            ViewBag.Mensagem = "Seja bem vindo! ";
+            return View();
+            // return Content("<h1>teste<h1>");
+        }
+    }
+}
